9b54a69368ae6922df73d9aeb20cbba3a4ed1071
Samuel Williams <samueltwilliams@gmail.com>
2012-04-25 07:55:42 +0100

    removed li dependence

--- www/js/saucer.js
+++ www/js/saucer.js
@@ -2,48 +2,11 @@ define(["jquery"], function($){
 
     "use strict";
     
-    var _update = function(where, names, root, listChange){
-        var dest  = root.find(where.where),
-            value = names[where.name];
-
-        // render a list
-        if(where.template) {
-
-            if(listChange) {
-                var from = listChange[0],
-                    to = listChange[1]
-
-                if(from < 0) {
-                    if(to == 0) {
-                        dest.find("li").eq(0).before(
-                            where.template.clone().text(value[to])
-                        )
-                    } else {
-                        dest.find("li").eq(to - 1).after(
-                            where.template.clone().text(value[to])
-                        )
-                    }
-                    
-                }
-                console.log(dest.length);
-            } else{
-                dest.empty();
-                $(value).each(function(i, o){
-                    dest.append(
-                        where.template.clone().text(o)
-                    )
-                })
-            }
-            
-
-        // jus a value 
-        } else {
-            dest.text(value);
-        }           
-    }
-
+    
     var Saucer = function(){
         var self = this;
+
+
 
         self.cup = function(node){
             var c = this,
@@ -52,22 +15,64 @@ define(["jquery"], function($){
                 root = $(node);
 
 
+            var _update = function(where, listChange){
+                var dest         = root.find(where.where),
+                    value        = names[where.name],
+                    itemSelector = where.itemSelector;
+
+                // render a list
+                if(where.template) {
+
+                    if(listChange) {
+                        var from = listChange[0],
+                            to = listChange[1]
+
+                        if(from < 0) {
+                            if(to == 0) {
+                                dest.find(itemSelector).eq(0).before(
+                                    where.template.clone().text(value[to])
+                                )
+                            } else {
+                                dest.find(itemSelector).eq(to - 1).after(
+                                    where.template.clone().text(value[to])
+                                )
+                            }
+                            
+                        }
+                    } else{
+                        dest.empty();
+                        $(value).each(function(i, o){
+                            dest.append(
+                                where.template.clone().text(o)
+                            )
+                        })
+                    }
+                    
+
+                // jus a value 
+                } else {
+                    dest.text(value);
+                }           
+            }
+
             c.where = function(selector){
                 var w = this,
-                    template;
+                    template,
+                    itemSelector;
                 
                 w.use = function(name){
                     wheres.push({
-                        where   : selector,
-                        name    : name,
-                        template: template
+                        where       : selector,
+                        name        : name,
+                        template    : template,
+                        itemSelector: itemSelector
                     });
                     return c;
                 }
 
-                w.items = function(itemSelector) {
+                w.items = function(item) {
+                    itemSelector = item;
                     template = root
-                        .find(selector)
                         .find(itemSelector)
                         .remove();
 
@@ -83,7 +88,7 @@ define(["jquery"], function($){
 
             c.render = function (){
                 $(wheres).each(function(index, where){
-                    _update(where, names, root);
+                    _update(where);
                 });
             }
 
@@ -91,7 +96,7 @@ define(["jquery"], function($){
                 wheres.filter(function(item){
                     return item.name == name;
                 }).forEach(function(where){
-                    _update(where, names, root, listChange);    
+                    _update(where, listChange);    
                 });
             }
 
