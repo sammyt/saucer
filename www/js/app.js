7ea7064e32064d409fda78a857cd76237590c37d
Samuel Williams <samueltwilliams@gmail.com>
2012-05-20 10:13:13 +0100

    remove old files

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,29 @@
-requirejs([], function () {
+requirejs(["saucer"], function (Cup) {
+
+     var cup = new Cup({
+        firstName     : "Sam" ,
+        lastName : "Williams" ,
+        friends  : [
+            { name  : "becky"
+            , type  : "wife"
+            },
+            { name  : "skyle"
+            , type  : "pet"
+            }
+        ]
+    })
+
+    var map = cup.map
+
+    map("firstName").to(".first-name")
+    map("lastName").to(".last-name")
+
+    map.each("friends")
+        .to("ul.friends", function(map) {
+            map("name").to(".friend-name")
+            map("type").to(".friend-type")
+        })
+
+    cup.touch()
 });
 
